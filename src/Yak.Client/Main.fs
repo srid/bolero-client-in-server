d32bf275d872f0f8f477a8f365702a368a0c5ef8
Sridhar Ratnakumar <srid@srid.ca>
2021-04-03 20:56:40 -0400

    init

---
+++ src/Yak.Client/Main.fs
@@ -0,0 +1,35 @@
+module Yak.Client.Main
+
+open Elmish
+open Bolero
+open Bolero.Html
+open Bolero.Templating.Client
+
+type Model =
+    {
+        x: string
+    }
+
+let initModel =
+    {
+        x = ""
+    }
+
+type Message =
+    | Ping
+
+let update message model =
+    match message with
+    | Ping -> model
+
+let view model dispatch =
+    text "Hello, world!"
+
+type MyApp() =
+    inherit ProgramComponent<Model, Message>()
+
+    override this.Program =
+        Program.mkSimple (fun _ -> initModel) update view
+#if DEBUG
+        |> Program.withHotReload
+#endif
